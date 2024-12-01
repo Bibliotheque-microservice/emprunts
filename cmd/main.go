b9b1f456d2f1321f79e644d3ebaaf7e04d61aa9b
Marie <mje.espinosa@gmail.com>
2024-12-01 19:40:53 +0100

    init rabbit MQ

--- cmd/main.go
+++ cmd/main.go
@@ -1,13 +1,46 @@
 package main
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"encoding/json"
+	"log"
+
+	rabbitmq "github.com/Bibliotheque-microservice/emprunts/rabbitMQ"
+	"github.com/Bibliotheque-microservice/emprunts/structures"
+	"github.com/gofiber/fiber/v2"
+)
 
 func main() {
+
+	rabbitmq.InitRabbitMQ()
+	defer rabbitmq.CloseRabbitMQ()
+
+	penality_msgs := rabbitmq.ConsumeMessages("user_penalties_queue")
+
+	for msg := range penality_msgs {
+		switch msg.RoutingKey {
+		case "user.v1.penalities.new":
+
+			// Parsing JSON and assign to jsonData
+			var jsonData structures.PenaltyMessage
+			err := json.Unmarshal(msg.Body, &jsonData)
+			if err != nil {
+				log.Printf("Erreur de parsing JSON : %v", err)
+				continue
+			}
+			log.Printf("Message JSON reçu : %+v", jsonData)
+			log.Print(jsonData.Amount)
+
+		case "user.v1.penalities.paid":
+			log.Printf("Message reçu : %s", string(msg.Body))
+		default:
+			log.Printf("Message non géré avec Routing Key : %s", msg.RoutingKey)
+			log.Printf("Contenu brut du message : %s", string(msg.Body))
+		}
+
+	}
 	app := fiber.New()
 
-	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("Hello, ma vie!")
-	})
+	setupRoutes(app)
 
 	app.Listen(":5000")
 }
